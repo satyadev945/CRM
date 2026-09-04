5fb353ad6ec6189a411c3987393ea11f958e4f1d
Modernize Studio <studio@modernize.com>
2026-09-04 08:57:34 +0000

    Commit for planId: 148

--- src/main/java/crm/repository/CustomerRepository.java
+++ src/main/java/crm/repository/CustomerRepository.java
@@ -19,13 +19,7 @@ import java.util.Set;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-    @Query(value = "select max(id) from crm.customer", nativeQuery = true)
-    Long getMaxId();
-
-    Iterable<Customer> findAllByEnabled(int enabled);
-
-    Customer findOneByEnabledAndName(int enabled, String name);
+    @Query(value = "select max(id) from customer", nativeQuery = true)
     Customer findOneByName(String name);
 
     Iterable<Customer> findByEnabledAndEmail(int enabled, String email);
